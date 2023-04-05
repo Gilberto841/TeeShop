09e48289831c2d7e4423b4a0381ae830cccbafb5
Gilberto841 <120499584+Gilberto841@users.noreply.github.com>
2023-04-05 14:53:46 +0200

    resgest

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -5,31 +5,81 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class,[
-            'label' => 'Email'
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide   : {{ value }}'
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'max' => 180,
+                        'minMessage'=> 'Votre email doit comporter au minumum {{ limit}} caractères. (email : {{ value }})',
+                        'maxMessage'=> 'Votre email doit comporter au maximum {{ limit}} caractères.'
+                    ]),
+
+                ],
             ])
             ->add('password', PasswordType::class,[
-                'label'=> 'Mot de passe'
-            ] )
+                'label'=> 'Mot de passe',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide : {{ value }}'
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'max' => 255,
+                        'minMessage'=> 'Votre email doit comporter au minumum {{ limit}} caractères.',
+                        'maxMessage'=> 'Votre email doit comporter au maximum {{ limit}} caractères.'
+                    ]),
+                ],
+            ])         
+
             ->add('firstname', TextType::class, [
-                'label' => 'prénom' 
-            ]) 
+                'label' => 'prénom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide : {{ value }}'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 100,
+                        'minMessage'=> 'Votre valeur doit comporter au minumum {{ limit}} caractères.',
+                        'maxMessage'=> 'Votre valeur doit comporter au maximum {{ limit}} caractères.'
+                    ]),
+                ],
+            ])          
+            
             ->add('lastname', TextType::class, [
-                'label' => 'Nom'
-            ])
-            ->add('gender', ChoiceType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Ce champ ne peut être vide : {{ value }}'
+                    ]),
+                    new Length([
+                        'min' => 1,
+                        'max' => 100,
+                        'minMessage'=> 'Votre valeur doit comporter au minumum {{ limit}} caractères.',
+                        'maxMessage'=> 'Votre valeur doit comporter au maximum {{ limit}} caractères.'
+                    ]),
+                ],
+            ])          
+            
+                ->add('gender', ChoiceType::class, [
                 'label' => 'Civilité',
                 'choices' => [
                     'Homme' => 'homme',
@@ -42,6 +92,11 @@ class RegisterFormType extends AbstractType
                 ],
                 'choice_attr' => [
                     'class' => 'radio-inline'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'ce champ ne peut être vide : {{ value }}'
+                    ])
                 ]
             ])
             
